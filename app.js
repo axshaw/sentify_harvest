4bf8598aa52d4647118f4bc6086c5ac0b01229da
Alex <alex@ctmlabs.io>
2014-06-02 08:55:53 +0100

    removed username from console. #nohopeforme

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ stream.on('tweet', function (tweet) {
             collection.insert(mongoDoc,function(err, result){
               if(err) console.log(err);
             });
-            console.log(tweet.user.screen_name +' : '+tweet.text);
+            console.log(tweet.probability +' : '+tweet.text);
             count++;
             updateCount(countFile,count);
           }else {
